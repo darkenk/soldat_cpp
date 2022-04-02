5c4df19ff707a050dca3d9c473c8225b8b9b8229
Dariusz Kluska <darkenk@gmail.com>
2022-04-02 18:21:48 +0200

    Change formatting for logger

--- src/common/Logging.cpp
+++ src/common/Logging.cpp
@@ -4,12 +4,15 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/logger.h>
+#include <spdlog/pattern_formatter.h>
 // clang-format on
 
 void InitLogging()
 {
+    spdlog::set_formatter(std::make_unique<spdlog::pattern_formatter>("[%H:%M:%S.%e][%n][%L] %v"));
     {
-        auto log = spdlog::default_logger();
+        auto log = spdlog::stdout_color_mt("global");
+        spdlog::set_default_logger(log);
         log->set_level(spdlog::level::debug);
     }
 
