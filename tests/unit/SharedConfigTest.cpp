b0f75a4d632924db094fa1dd0dc3b7d89268bdff
Dariusz Kluska <darkenk@gmail.com>
2022-04-09 17:52:35 +0200

    Fix test compilation

--- tests/unit/SharedConfigTest.cpp
+++ tests/unit/SharedConfigTest.cpp
@@ -43,58 +43,54 @@ Chat_SeeEnemy=Drop your gun
 TEST(SharedConfigTest, SpritecHasSetBotControlMethod)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   EXPECT_EQ(true, loadbotconfig(tf, sprite, guns));
   EXPECT_EQ(bot, sprite.player->controlmethod);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ReadFavouriteWeapon)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
   EXPECT_EQ(true, loadbotconfig(tf, sprite, guns));
   EXPECT_EQ(ruger77_num, sprite.brain.favweapon);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ReadSecondaryWeapon)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
   EXPECT_EQ(true, loadbotconfig(tf, sprite, guns));
   EXPECT_EQ(0, sprite.player->secwep);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, FriendValueCanBeEmpty)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
   EXPECT_EQ(true, loadbotconfig(tf, sprite, guns));
   EXPECT_EQ("", sprite.brain.friend_);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ShootSettings)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
@@ -104,14 +100,13 @@ TEST(SharedConfigTest, ShootSettings)
   EXPECT_EQ(0, sprite.brain.deadkill);
   EXPECT_EQ(500, sprite.brain.grenadefreq);
   EXPECT_EQ(255, sprite.brain.use);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ParseChatSettings)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
@@ -122,28 +117,26 @@ TEST(SharedConfigTest, ParseChatSettings)
   EXPECT_EQ("", sprite.brain.chatlowhealth);
   EXPECT_EQ("Drop your gun", sprite.brain.chatseeenemy);
   EXPECT_EQ("", sprite.brain.chatwinning);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ReadNameCamper)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
   EXPECT_EQ(true, loadbotconfig(tf, sprite, guns));
   EXPECT_EQ(255, sprite.brain.camper);
   EXPECT_STREQ("Kruger", sprite.player->name.c_str());
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ReadColor)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   sprite.player->team = team_none;
   GunsDescription guns;
   GunsDescription defaultguns;
@@ -153,14 +146,13 @@ TEST(SharedConfigTest, ReadColor)
   EXPECT_EQ(0x0097C69D, sprite.player->pantscolor);
   EXPECT_EQ(0x00E6B478, sprite.player->skincolor);
   EXPECT_EQ(0x00515C79, sprite.player->haircolor);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ReadHeadConfig)
 {
   TIniFile tf{ReadAsMemoryStream(BotIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
@@ -170,20 +162,18 @@ TEST(SharedConfigTest, ReadHeadConfig)
   EXPECT_EQ(0, sprite.wearhelmet);
   EXPECT_EQ(0, sprite.player->chain);
   EXPECT_EQ(false, sprite.dummy);
-  delete sprite.player;
 }
 
 TEST(SharedConfigTest, ReadInvalidOptionsReturnFalse)
 {
   auto invalidIni = R"([BOT])";
   TIniFile tf{ReadAsMemoryStream(invalidIni)};
-  tsprite sprite;
-  sprite.player = new tplayer();
+  tsprite sprite(1);
+  sprite.player = std::make_shared<tplayer>();
   GunsDescription guns;
   GunsDescription defaultguns;
   createweapons(false, guns, defaultguns);
   EXPECT_EQ(false, loadbotconfig(tf, sprite, guns));
-  delete sprite.player;
 }
 
 auto constexpr WeaponConfig = R"(
